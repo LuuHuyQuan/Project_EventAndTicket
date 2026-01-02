113e3ee2263b034b3a45e1668c93d8e6afa04bd3
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2026-01-02 23:48:14 +0700

    Update âôăô

--- Project_ApiEventTicket/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Project_ApiEventTicket/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675d595df24567dcc8b842f7deda144f7b630610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497cf834b9db4a1720f073e36c48d14674bcfb5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
