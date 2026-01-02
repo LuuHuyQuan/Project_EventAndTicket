68dec579b50dd66824a804ddd331152047b7ace4
Lưu Huy Quân <luuhuyquandt05@gmail.com>
2026-01-02 23:00:29 +0700

    Create Repositories

--- Project_ApiEventTicket/Event_Management.Attendee/obj/Debug/net8.0/Event_Management.Attendee.AssemblyInfo.cs
+++ Project_ApiEventTicket/Event_Management.Attendee/obj/Debug/net8.0/Event_Management.Attendee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event_Management.Attendee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5552a5d63d11d90da7e9c7b1c0d8b1f58134806f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb7f43f067b4dba0ebd20b8768e0f5a525c6896")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event_Management.Attendee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event_Management.Attendee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
